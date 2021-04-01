8f4a23c115525c36177138e4ec915998e7f52494
CuyaQM4 <yangyao2132edb@126.com>
2021-04-01 18:00:04 +0800

    修改

---
+++ app/src/main/java/com/example/planegameclient/util/constant.java
@@ -0,0 +1,13 @@
+package com.example.planegameclient.util;
+
+/**
+ * Created by CNH on 2016/10/14.
+ */
+
+public class Constant {
+    public static final int radio=80;//半径
+    public static final int JK_Start_x=30;//摇杆大环起点x
+    public static final int JK_Start_y=830;//摇杆大环起点y
+    public static final int  xJoystick=100;//摇杆小环x
+    public static final int  yJoystick=900;//摇杆小环y
+}
